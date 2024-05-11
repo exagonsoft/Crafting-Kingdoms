d59c15c17cabab2e3c669d979841e1e5ddf924eb
melkorAMP <melkor@aleph.engineering>
2024-05-11 16:39:59 -0400

    #31 Update Kingdom Overall component
    - Implement handlers on card owned cards components

--- components/WorkShops.tsx
+++ components/WorkShops.tsx
@@ -1,19 +1,60 @@
 import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
-import React from "react";
-import { STAKING_CONTRACT_ADDRESS } from "../constants/contracts";
+import React, { useEffect, useState } from "react";
+import { buildingsIds, STAKING_CONTRACT_ADDRESS } from "../constants/contracts";
 import styles from "../styles/Home.module.css";
 import { BigNumber } from "ethers";
 import WorkShopCard from "./WorkShopCard";
 import LoadingAnimation from "./LoadingAnimator";
 
-const WorkShops = ({ onEarningsClaimed, setBusy }) => {
+const WorkShops = ({ onEarningsClaimed, setBusy, onPropertyChange }) => {
   const address = useAddress();
   const { contract: stakingContract } = useContract(STAKING_CONTRACT_ADDRESS);
+  const [economyBuildings, setEconomyBuildings] = useState([
+    {
+      buildingID: Number(),
+      quantity: Number(),
+    },
+  ]);
   const { data: stakedTokens, isLoading: loadingWorkShops } = useContractRead(
     stakingContract,
     "getStakeInfo",
     [address]
   );
+
+  const updateBuildingsCount = (tokenId: number, quantity: number) => {
+    let _economyBuildings = economyBuildings;
+    _economyBuildings.map((building) => {
+      if (building.buildingID === tokenId) {
+        building.quantity = quantity;
+      }
+    });
+    let _buildings = 0;
+    let _hasTavern = false;
+    _economyBuildings.map((building) => {
+      _buildings += building.quantity;
+      if(building.buildingID === buildingsIds.Tavern){
+        _hasTavern = true;
+      }
+    });
+    onPropertyChange("economyBuildings", _buildings);
+  };
+
+  useEffect(() => {
+    if (stakedTokens && stakedTokens[0].length > 0) {
+      let _economyBuildings = economyBuildings;
+      stakedTokens[0].map((stakedToken: BigNumber) => {
+        let _exist = _economyBuildings.filter((object) => object.buildingID === stakedToken.toNumber())
+        if(_exist.length === 0){
+          _economyBuildings.push({
+            buildingID: stakedToken.toNumber(),
+            quantity: 0,
+          });
+        }
+      });
+      setEconomyBuildings(_economyBuildings);
+    }
+  }, []);
+
   return (
     <div className={styles.business_section}>
       <div className={styles.businessContainer}>
@@ -38,6 +79,7 @@ const WorkShops = ({ onEarningsClaimed, setBusy }) => {
                     tokenId={stakedToken.toNumber()}
                     claimEarnings={onEarningsClaimed}
                     onBusy={setBusy}
+                    onPropertyChange={updateBuildingsCount}
                   />
                 ))
               ) : (
@@ -65,14 +107,7 @@ const WorkShops = ({ onEarningsClaimed, setBusy }) => {
             </h3>
             <div className={styles.grid}>
               {stakedTokens && stakedTokens[0].length > 0 ? (
-                stakedTokens[0].map((stakedToken: BigNumber) => (
-                  <WorkShopCard
-                    key={stakedToken.toString()}
-                    tokenId={stakedToken.toNumber()}
-                    claimEarnings={onEarningsClaimed}
-                    onBusy={setBusy}
-                  />
-                ))
+                stakedTokens[0].map((stakedToken: BigNumber) => <></>)
               ) : (
                 <p>No outposts owned...</p>
               )}
@@ -98,14 +133,7 @@ const WorkShops = ({ onEarningsClaimed, setBusy }) => {
             </h3>
             <div className={styles.grid}>
               {stakedTokens && stakedTokens[0].length > 0 ? (
-                stakedTokens[0].map((stakedToken: BigNumber) => (
-                  <WorkShopCard
-                    key={stakedToken.toString()}
-                    tokenId={stakedToken.toNumber()}
-                    claimEarnings={onEarningsClaimed}
-                    onBusy={setBusy}
-                  />
-                ))
+                stakedTokens[0].map((stakedToken: BigNumber) => <></>)
               ) : (
                 <p>No heroes owned...</p>
               )}
@@ -131,14 +159,7 @@ const WorkShops = ({ onEarningsClaimed, setBusy }) => {
             </h3>
             <div className={styles.grid}>
               {stakedTokens && stakedTokens[0].length > 0 ? (
-                stakedTokens[0].map((stakedToken: BigNumber) => (
-                  <WorkShopCard
-                    key={stakedToken.toString()}
-                    tokenId={stakedToken.toNumber()}
-                    claimEarnings={onEarningsClaimed}
-                    onBusy={setBusy}
-                  />
-                ))
+                stakedTokens[0].map((stakedToken: BigNumber) => <></>)
               ) : (
                 <p>No heroes owned...</p>
               )}
---
+++ interfaces/kingdomInterface.ts
@@ -0,0 +1,10 @@
+export type kingdomOverallInterface = {
+  economyBuildings: number,
+  militaryBuildings: number,
+  heroes: number,
+  items: number,
+  tavern: boolean,
+  magicTower: boolean,
+  swordManTower: boolean,
+  archersTower: boolean,
+};
